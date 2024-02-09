6b608b32654e37f3a0094ba85d445c830a1d8097
Rolando <87973999+Rgzsat@users.noreply.github.com>
2024-02-09 17:00:56 +0200

    Update Bayes.py

--- Supervised Learning/Classification/Bayes.py
+++ Supervised Learning/Classification/Bayes.py
@@ -34,7 +34,6 @@ def initial_calc(X,y, classes):
 f_pos= initial_calc(X, y, classes)
 
 #%%
-     
 def calculate_likelihood(class_idx, x):
     mean= f_pos[0][0:2][class_idx]
     #mean = ini_mean[class_idx]
@@ -63,3 +62,32 @@ def naive_predict(X):
     return np.array(y_pred)
 
 #%%
+yp =naive_predict(X_test)
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+# Calculate the confusion matrix
+#
+conf_matrix = confusion_matrix(y_true=y_test, y_pred=np.array(yp))
+#
+# Print the confusion matrix using Matplotlib
+fig, ax = plt.subplots(figsize=(5, 5))
+ax.matshow(conf_matrix, cmap=plt.cm.Oranges, alpha=0.3)
+for i in range(conf_matrix.shape[0]):
+    for j in range(conf_matrix.shape[1]):
+        ax.text(x=j, y=i,s=conf_matrix[i, j], va='center', ha='center', size='xx-large')
+ 
+plt.xlabel('Predictions', fontsize=18)
+plt.ylabel('Actuals', fontsize=18)
+plt.title('Confusion Matrix- Näive Bayes', fontsize=18)
+plt.show()
+
+y_true= y_test
+print('accuracy is', accuracy_score(y_true, yp))
+print('f1 score is', f1_score(y_true,yp, average=None)) 
+print('sensitivity is',recall_score(y_true, yp, pos_label=1))
+print('specificity is', recall_score(y_true, yp, pos_label=0))
+print('precision score is', precision_score(y_true, yp, pos_label= 1))
+print('The ROC-AUC score of the model is:', round(roc_auc_score(y_test, yp), 4))
+
