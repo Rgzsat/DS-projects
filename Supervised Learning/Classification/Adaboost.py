9788adcf2f24754f27bfb7129cf19834e058d3b9
Rolando <87973999+Rgzsat@users.noreply.github.com>
2024-04-22 11:53:27 +0300

    Update Adaboost.py

--- Supervised Learning/Classification/Adaboost.py
+++ Supervised Learning/Classification/Adaboost.py
@@ -11,3 +11,20 @@ from sklearn.metrics import roc_auc_score
 X, y = datasets.make_classification(n_samples=1000, n_features=20, n_classes=2, random_state=42)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=5)
 
+def calc_error(y, y_pred, w_ind):
+    '''
+    Calculate the error rate of a weak classifier
+    y: target variable
+    y_pred: predicted variable by weak classifier
+    w_ind: individual weights for each observation
+        '''
+    error= (sum(w_ind * (np.not_equal(y, y_pred)).astype(int)))/sum(w_ind)
+    return error
+
+def calc_alpha(error):
+    '''
+    Through majority vote of the final classifier, it calculate the weight of a weak classifier.
+    error: error rate from weak classifier
+    '''
+    alpha=np.log((1 - error) / error)
+    return alpha
