eb20480a6e8a806632f960f4bf9b6de731ccec89
Rolando <87973999+Rgzsat@users.noreply.github.com>
2025-09-29 22:00:21 +0300

    Update Battery-ANN.py

--- Deep Learning/Projects/Battery-ANN.py
+++ Deep Learning/Projects/Battery-ANN.py
@@ -1,1 +1,10 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import keras_tuner as kt
+import matplotlib.pyplot as plt
 
