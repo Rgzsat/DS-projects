40ec54bca915d9975baa02f443e53fb2063b84e9
Rolando <87973999+Rgzsat@users.noreply.github.com>
2025-09-30 13:20:52 +0300

    Update Battery-ANN.py

--- Deep Learning/Projects/Battery-ANN.py
+++ Deep Learning/Projects/Battery-ANN.py
@@ -1,10 +1,18 @@
-import numpy as np
 import pandas as pd
-from sklearn.model_selection import KFold
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
-import keras_tuner as kt
-import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.regularizers import l2
+
+# -------------------------------------
+# TRANSFORM FUNCTION
+# -------------------------------------
 
