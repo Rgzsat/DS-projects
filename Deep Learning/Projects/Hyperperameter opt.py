cfa7be7a64fc290e545dede1a9304c3be693bfa6
Dr. Rolando <87973999+Rgzsat@users.noreply.github.com>
2025-10-06 16:53:32 +0300

    Update Hyperperameter opt.py

--- Deep Learning/Projects/Hyperperameter opt.py
+++ Deep Learning/Projects/Hyperperameter opt.py
@@ -21,3 +21,29 @@ dataset_valid= "INSERT YOUR VALIDATION PATH HERE"
 
 dataset_test="INSERT YOUR TESTING PATH HERE"
 
+#OPTIONAL, IF YOU HAVE LIMITED DATASETS
+
+train_size = int(len(dataset_train) * 0.70)
+val_size= int(len(dataset_train) * 0.20)
+test_size= int(len(dataset_train) * 0.10)
+scaler = StandardScaler()
+
+def get_val(dataset):
+    dataset=(dataset.drop('time', axis=1))
+    cap= dataset['mAh']/1000
+    max_cap=cap[len(cap)-1]
+    dod= (max_cap - cap)/max_cap
+    dataset['SOC']= dod
+    X= dataset
+    X= scaler.fit_transform(X)
+    
+    ir= (4.1-dataset['V'])/dataset['I']
+    yi= ir[0]*dataset['I']+dataset['V']
+    y = np.array(yi)
+    
+    return X,y
+
+
+X_train, y_train=get_val(dataset_train)[0], get_val(dataset_train)[1]
+X_val, y_val=get_val(dataset_valid)[0], get_val(dataset_valid)[1]
+X_test, y_test=get_val(dataset_test)[0], get_val(dataset_test)[1]
