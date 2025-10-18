1cf6284b13ce94c8ed623c592916f31d74f01479
Dr. Rolando <87973999+Rgzsat@users.noreply.github.com>
2025-10-18 12:36:47 +0300

    Update Bidirectional NN.py

--- Deep Learning/Projects/Bidirectional NN.py
+++ Deep Learning/Projects/Bidirectional NN.py
@@ -178,3 +178,9 @@ plt.ylabel('Voltage [V]')
 plt.title('Bidirectional implementation-Validation')
 plt.legend()
 plt.show()
+
+#%%
+
+dataset_test= pd.read_csv(r'C:\Users\rogilb\Downloads\Testing cells\10A-2-A-4.csv',index_col= 6)#[:-1]
+df_test= df(dataset_test)
+X_test, Y_test = create_dataset(df_test, look_back)
