7ca556713a95bef400b888fcaad2ae0f21575f5f
Dr. Rolando <87973999+Rgzsat@users.noreply.github.com>
2025-10-23 23:13:06 +0300

    Update BiGRU-template.py

--- Deep Learning/Projects/BiGRU-template.py
+++ Deep Learning/Projects/BiGRU-template.py
@@ -6,3 +6,16 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import keras
 from keras.callbacks import EarlyStopping
+
+from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
+from tensorflow.keras import backend as K
+from tensorflow.keras.models import Sequential
+#from tensorflow.keras.layers import Dense, LSTM
+from tensorflow.keras.layers import Dense, LSTM, Dropout, GRU, Bidirectional
+
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import load_model
+from keras.layers import  Dropout, Flatten
+
+from keras.layers import Activation, Dense
