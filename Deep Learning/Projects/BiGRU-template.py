e744b8f13f93898c35fffe47be9852a080060662
Dr. Rolando <87973999+Rgzsat@users.noreply.github.com>
2025-10-23 23:13:46 +0300

    Update BiGRU-template.py

--- Deep Learning/Projects/BiGRU-template.py
+++ Deep Learning/Projects/BiGRU-template.py
@@ -19,3 +19,15 @@ from tensorflow.keras.models import load_model
 from keras.layers import  Dropout, Flatten
 
 from keras.layers import Activation, Dense
+
+import skopt
+from sklearn.preprocessing import MinMaxScaler
+from skopt import gp_minimize, forest_minimize
+from skopt.space import Real, Integer 
+from skopt.plots import plot_convergence, plot_objective, plot_evaluations, plot_histogram, plot_objective_2D 
+from skopt.utils import use_named_args
+
+dataset_train = pd.read_csv(r'C:\Users\47406\Downloads\Coding-initial\1B4-1A.csv', index_col= 6)[:-1]
+dataset_valid= pd.read_csv(r'C:\Users\47406\Downloads\Coding-initial\1A1-10A.csv', index_col= 6)#[:-1]
+#sc = StandardScaler()
+scaler = MinMaxScaler(feature_range=(0, 1))
