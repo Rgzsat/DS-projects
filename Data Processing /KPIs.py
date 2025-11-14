5f4801bdd6d2b24d10c3e9e3f919381879d7178a
Dr. Rolando <87973999+Rgzsat@users.noreply.github.com>
2025-11-14 13:39:24 +0200

    Update KPIs.py

--- Data Processing /KPIs.py
+++ Data Processing /KPIs.py
@@ -4,7 +4,7 @@ import numpy as np
 # import matplotlib.pyplot as plt
 import os
 import sys
-sys.path.append(r'C:\Users\47406\Downloads\Coding 2023\Article- Neural networks\Neural networks- literature\feature_selection')
+sys.path.append(r'C:\Users\Downloads\Coding 2023\Article- Neural networks\Neural networks- literature\feature_selection')
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Lasso, LogisticRegression
