5e947b7d9afdde1be7bb79309c64d186ba6c3c1e
Dr. Rolando <87973999+Rgzsat@users.noreply.github.com>
2025-12-13 11:27:51 -0600

    Update Test-CV.py

--- Deep Learning/Projects/Cross validation/Test-CV.py
+++ Deep Learning/Projects/Cross validation/Test-CV.py
@@ -3,3 +3,9 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import glob
+
+path = # use your path
+#all_files = glob.glob(os.path.join(path , "/*.csv"))
+files = Path(path).glob('*.csv')  # .rglob to get subdirectories
+from sklearn.preprocessing import MinMaxScaler
+
