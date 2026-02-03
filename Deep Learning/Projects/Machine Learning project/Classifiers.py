26e56afca1862ce7f708f23164acb67904c1f85c
Dr. Rolando <87973999+Rgzsat@users.noreply.github.com>
2026-02-03 22:44:06 +0200

    Update Classifiers.py

--- Deep Learning/Projects/Machine Learning project/Classifiers.py
+++ Deep Learning/Projects/Machine Learning project/Classifiers.py
@@ -268,7 +268,7 @@ voltage_df = pd.DataFrame(results_voltage)
 res_df = pd.DataFrame(results_resistance)
 
 voltage_df.to_csv("NAME OF YOUR OUTPUT FILE.csv", index=False)
-res_df.to_csv("2023-2024-LEV50N-internal_resistance_diagnostics.csv", index=False)
+res_df.to_csv("NAME OF YOUR OUTPUT FILE.csv", index=False)
 
 print(" Saved results:")
 print("   • voltage_soc_diagnostics.csv")
