f7420abc27c7d760a2be0eb7085941cfbf81d50e
Dr. Rolando <87973999+Rgzsat@users.noreply.github.com>
2026-02-23 10:44:14 +0200

    Create Test-Classifiers.py

---
+++ Deep Learning/Projects/Machine Learning project/Test-Classifiers.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (
+    classification_report, accuracy_score, roc_auc_score,
+    confusion_matrix, precision_recall_curve, fbeta_score
+)
+from sklearn.feature_selection import mutual_info_classif
+from imblearn.over_sampling import SMOTE
